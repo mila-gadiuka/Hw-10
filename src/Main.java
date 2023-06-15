b7ed7022bfe97bd782c4addbce822983a604e8ee
milena <miladar050813@gmail.com>
2023-06-15 18:52:50 +0300

    База для кода

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import com.sun.source.tree.UsesTree;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+
+        task1();
+        task2();
+        task3();
+
+        scanner.close();
+    }
+
+    public static void task1() {
+
+    }
+
+    public static void task2() {
+
+    }
+
+    public static void task3() {
+
+    }
+}
